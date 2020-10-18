8b1cc6c252c34e38f3292b6823c51572ea2b3848
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-10-19 11:01:46 +1300

    WIP on CaseStudy

--- components/CaseStudies.tsx
+++ components/CaseStudies.tsx
@@ -1,9 +1,47 @@
 /** @jsx jsx */
-import { jsx, Flex, Box, Heading, Text, Button } from 'theme-ui'
+import { jsx, Flex, Box, Heading, Text, Image, Button } from 'theme-ui'
 
 import FullWidthCentered from './FullWidthCentered'
 import OneThenTwoColumns from './OneThenTwoColumns'
 import ReportCircle from './ReportCircle'
+
+function CaseStudy () {
+  return (
+    <Flex sx={{ flexDirection: 'column', width: 500 }}>
+      <Flex sx={{ position: 'relative' }}>
+        <Heading
+          variant='h2'
+          sx={{ position: 'absolute', bottom: 2, left: 3, color: 'white' }}
+        >
+          MEVO
+        </Heading>
+        <Image src='/images/case-study-banner-mevo.png' />
+      </Flex>
+
+      <Flex p={3} sx={{ flexDirection: 'column' }}>
+        <Flex>
+          <Text>
+            Mevo is Australasia's first car-share that isn’t tied to one
+            particular parking spot. Mevo is changing how we move by providing a
+            better alternative to private car ownership.
+          </Text>
+          <Image
+            src='/images/case-study-logo-mevo.png'
+            ml={3}
+            sx={{ width: 7 }}
+          />
+        </Flex>
+        <Flex>
+          <Button>DOWNLOAD FULL</Button>
+          <Flex>
+            <Box>CLIMATE</Box>
+            <Box>ICON</Box>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  )
+}
 
 interface CaseStudiesProps {}
 
@@ -23,7 +61,9 @@ export default function CaseStudies ({}: CaseStudiesProps) {
           }
         />
 
-        <Flex></Flex>
+        <Flex>
+          <CaseStudy />
+        </Flex>
       </Flex>
     </FullWidthCentered>
   )
