4fd3aecbe4a309f7b72263a947008f29b9528650
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-10-21 18:51:51 +1300

    waste page

---
+++ pages/waste.tsx
@@ -0,0 +1,118 @@
+/** @jsx jsx */
+import { jsx, Flex, Box, Heading, Text, Image } from 'theme-ui'
+import { Airtable } from '@bamboocreativenz/pip-airtable'
+import keyBy from 'lodash/keyBy'
+
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
+import FullWidthCentered from '../components/FullWidthCentered'
+import OneThenTwoColumns from '../components/OneThenTwoColumns'
+import IndustryReports from '../components/IndustryReports'
+import CaseStudies from '../components/CaseStudies'
+import AccreditorsAndCertifications from '../components/AccreditorsAndCertifications'
+import Latest from '../components/Latest'
+
+interface WasteProps {
+  caseStudies: any // TODO: type better
+  accreditors: any // TODO: type better
+  waste: any // TODO: type better
+}
+
+export default function Waste ({
+  caseStudies,
+  accreditors,
+  waste
+}: WasteProps) {
+  return (
+    <Flex sx={{ flexDirection: 'column' }}>
+      <Banner
+        backgroundImage={waste.Banner.Image[0].url}
+        headline={waste.Banner.Title}
+        subHeadline={waste.Banner.Content}
+      />
+
+      <FullWidthCentered>
+        <Flex px={[3, 5]} mb={[3, 5]} sx={{ flexDirection: 'column' }}>
+          <OneThenTwoColumns
+            mt={5}
+            firstColumnContent={
+              <Heading variant='h1'>{waste.What.Title}</Heading>
+            }
+            remainingContent={
+              <Flex ml={[0, 4]} mt={[3, 0]} sx={{ flexDirection: 'column' }}>
+                <Text
+                  variant='p2'
+                  sx={{ whiteSpace: 'pre-wrap' }}
+                  dangerouslySetInnerHTML={{ __html: waste.What.Content }}
+                />
+              </Flex>
+            }
+          />
+
+          <OneThenTwoColumns
+            mt={5}
+            firstColumnContent={
+              <Heading variant='h1'>{waste.Why.Title}</Heading>
+            }
+            remainingContent={
+              <Flex ml={[0, 4]} mt={[3, 0]} sx={{ flexDirection: 'column' }}>
+                <Text
+                  variant='p2'
+                  sx={{ whiteSpace: 'pre-wrap' }}
+                  dangerouslySetInnerHTML={{ __html: waste.Why.Content }}
+                />
+              </Flex>
+            }
+          />
+        </Flex>
+        <Image src={waste.Why.Image[0].url} />
+      </FullWidthCentered>
+
+      <IndustryReports copy={waste['Industry Reports']} />
+
+      <CaseStudies caseStudies={caseStudies} copy={waste['Case Studies']} />
+
+      <AccreditorsAndCertifications
+        accreditors={accreditors}
+        copy={waste.Accreditors}
+      />
+
+      <Latest copy={waste.Latest} />
+
+      <Footer />
+    </Flex>
+  )
+}
+
+export async function getStaticProps (context) {
+  const airtable = new Airtable(
+    process.env.AIRTABLE_API_KEY,
+    process.env.AIRTABLE_BASE
+  )
+  const caseStudiesRecords = await airtable.listRecords({
+    tableName: 'Case Studies',
+    viewName: 'Grid View'
+  })
+  const caseStudies = caseStudiesRecords.map(c => c.fields)
+  const accreditorsRecords = await airtable.listRecords({
+    tableName: 'Accreditors',
+    viewName: 'Grid View'
+  })
+  const accreditors = accreditorsRecords.map(c => c.fields)
+  const wasteRecords = await airtable.listRecords({
+    tableName: 'Waste Page',
+    viewName: 'Grid View'
+  })
+  const waste = keyBy(
+    wasteRecords.map(c => c.fields),
+    'Name'
+  )
+
+  return {
+    props: {
+      caseStudies,
+      accreditors,
+      waste
+    }
+  }
+}
