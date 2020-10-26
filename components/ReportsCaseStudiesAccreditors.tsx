4c18217309068ff7fb812ee7f3febb472f6755f5
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-10-27 10:59:17 +1300

    add ReportsCaseStudiesAccreditors

---
+++ components/ReportsCaseStudiesAccreditors.tsx
@@ -0,0 +1,50 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+
+import IndustryReports from './IndustryReports'
+import CaseStudies from './CaseStudies'
+import AccreditorsAndCertifications from './AccreditorsAndCertifications'
+import Latest from './Latest'
+import Footer from './Footer'
+
+export default function ReportsCaseStudiesAccreditors ({
+  page,
+  selectedIndustry,
+  setSelectedIndustry,
+  selectedTheme,
+  setSelectedTheme,
+  marketInsights,
+  caseStudies,
+  accreditors
+}) {
+  return (
+    <>
+      <IndustryReports
+        copy={page['Industry Reports']}
+        marketInsights={marketInsights}
+        selectedIndustry={selectedIndustry}
+        setSelectedIndustry={setSelectedIndustry}
+        selectedTheme={selectedTheme}
+        setSelectedTheme={setSelectedTheme}
+      />
+
+      <CaseStudies
+        caseStudies={caseStudies}
+        copy={page['Case Studies']}
+        selectedIndustry={selectedIndustry}
+        selectedTheme={selectedTheme}
+      />
+
+      <AccreditorsAndCertifications
+        accreditors={accreditors}
+        copy={page.Accreditors}
+        selectedIndustry={selectedIndustry}
+        selectedTheme={selectedTheme}
+      />
+
+      <Latest copy={page.Latest} />
+
+      <Footer />
+    </>
+  )
+}
