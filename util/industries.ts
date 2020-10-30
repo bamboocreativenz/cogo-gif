7c79df639509435c24c8859df9f4c8806efdf39c
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2020-10-31 09:46:19 +1300

    typo

--- util/industries.ts
+++ util/industries.ts
@@ -8,7 +8,7 @@ export default [
   { name: 'Fashion', icon: require('../public/icons/fashion.png') },
   { name: 'Baby & Child', icon: require('../public/icons/baby-and-child.png') },
   { name: 'Utilities', icon: require('../public/icons/utilities.png') },
-  { name: 'Grocery', icon: require('../public/icons/Grocery.png') },
+  { name: 'Grocery', icon: require('../public/icons/grocery.png') },
   { name: 'Activities', icon: require('../public/icons/activities.png') },
   {
     name: 'Homeware & Gifts',
